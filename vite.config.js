2141dc6a247f7b7969c8e7b51f7a90dbe9a8b474
abrar <abrar53450@engmet.edu.eg>
2025-12-25 18:26:15 +0200

    Prepare project for GitHub Pages with HashRouter

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,8 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/reactproject1/',
+  base: '/reactproject1/', // ضع هنا اسم الريبو الخاص بك
+  build: {
+    outDir: 'docs' // ننشر مباشرة من مجلد docs لتسهيل GitHub Pages
+  }
 })
