4bee3fbf7b65678f23ce63e3ee78e31aa9f205a3
abrar <abrar53450@engmet.edu.eg>
2025-12-25 18:20:41 +0200

    Use HashRouter and update build for GitHub Pages

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,14 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
 import './output.css'
+import App from './App.jsx'
+import { HashRouter } from 'react-router-dom'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
+    <HashRouter basename="/reactproject1">
+      <App />
+    </HashRouter>
+  </StrictMode>
 )
