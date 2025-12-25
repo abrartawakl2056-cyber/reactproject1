3cf1962740e78c9e695095c549ccb267058a7894
abrar <abrar53450@engmet.edu.eg>
2025-12-25 19:57:08 +0200

    Fix routing issue and set Base as homepage

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,22 @@
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
-import './App.css'
 import Layout from './Layout/Layout'
 import Base from './Base/Base'
 import About from './About/About'
 import Portofolio from './Portfolio/Portofolio'
 import Contact from './Contact/Contact'
 
-const route = createBrowserRouter(
-  [
-    {
-      path: "/",
-      element: <Layout />,
-      children: [
-        { index: true, element: <Base /> },
-        { path: "about", element: <About /> },
-        { path: "portofolio", element: <Portofolio /> },
-        { path: "contact", element: <Contact /> }
-      ]
-    }
-  ],
- 
-)
+const route = createBrowserRouter([
+  {
+    path: "/reactproject1",
+    element: <Layout />,
+    children: [
+      { index: true, element: <Base /> },
+      { path: "about", element: <About /> },
+      { path: "portofolio", element: <Portofolio /> },
+      { path: "contact", element: <Contact /> }
+    ]
+  }
+])
 
 function App() {
   return <RouterProvider router={route} />
--- src/main.jsx
+++ src/main.jsx
@@ -3,12 +3,11 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import './output.css'
 import App from './App.jsx'
-import { HashRouter } from 'react-router-dom'
+import { BrowserRouter, HashRouter } from 'react-router-dom'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <HashRouter basename="/reactproject1">
       <App />
-    </HashRouter>
+    
   </StrictMode>
 )
